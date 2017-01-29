e0d73d92976038b3b33bfa014e268c5004d79a6e
shawn-southwell <shawn.southwel@gmail.com>
2017-01-28 19:03:20 -0800

    fixing sequelize error

---
+++ client/src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+//no reducers yet. 
+import mainReducer from './Reducers/reducer';
+import ReduxPromise from 'redux-promise';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const middleware = applyMiddleware(ReduxPromise, thunk, logger());
+export default createStore(mainReducer , middleware);
