4c952897813ac04df52672ec88b89c1410ea9ba1
shawn-southwell <shawn.southwel@gmail.com>
2017-02-01 02:36:21 -0800

    Clean up admin controller whitespace

--- database/controllers/adminController.js
+++ database/controllers/adminController.js
@@ -4,48 +4,42 @@ const adminController = {};
 
 adminController.addAdmin = (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).end();
-  } 
+
+  if (!username || !password) return res.status(400).json({ err: 'Please supply a valid username and password' }); 
+
   Admin.create({ username, password })
-       .then(() => { 
-         console.log('ADMIN ADDED UP IN THAT DB DOE'); 
-         res.end();
-        })
-       .catch(err => {
-         console.error(err)
-         res.status(500).end();
-       });
+   .then(() => res.end();)
+   .catch(err => res.status(500).json({ err }));
+
 };
 
 adminController.findAdmin = (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
-    console.log("username:", username, "password:", password);
-    return res.status(400).end();
-  } 
+
+  if (!username || !password) return res.status(400).json({ err: 'Please supply a valid username and password' }); 
 
   Admin.findOne({where: {username, password }})
-        .then(userExists => userExists ? res.end() : res.sendStatus(400).end()) 
-        .catch(() => {
-          console.log("CATCHNINGNGNG")
-          res.sendStatus(400)
-        });
+    .then(userExists => userExists ? res.end() : res.sendStatus(400).end()) 
+    .catch(() => res.sendStatus(400));
+
 }
 
 adminController.removeAdmin = (req, res) => {
  const { username, password } = req.body;
+
  Admin.destroy({ where : { username, password }})
-			.then(() => res.sendStatus(201))
-			.catch(() => res.sendStatus(404));
+  .then(() => res.sendStatus(201))
+  .catch(() => res.sendStatus(404));
+
 }
 
 adminController.changePassword = (req, res) => {
 	const { username, password, newpassword } = req.body;
-	Admin.update({ password: newpassword },
-				{ username, password })
-			 .then(() => res.sendStatus(200))
-			 .catch(() => res.sendStatus(404));
+
+	Admin.update({ password: newpassword },{ username, password })
+   .then(() => res.sendStatus(200))
+   .catch(() => res.sendStatus(404));
+
 }
 
 
